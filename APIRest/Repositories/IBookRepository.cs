952a268ac9d26a1dd8b0ed1d2cac98780316f060
LopezSanty <slopezr10@miumg.edu.gt>
2021-08-24 16:49:29 -0600

    Add project files.

---
+++ APIRest/Repositories/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using APIRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+        Task<Book> Get(int id);
+        Task<Book> Create(Book book);
+        Task Update(Book book);
+        Task Delete(int id);
+    }
+}
